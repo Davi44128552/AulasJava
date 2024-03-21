550ec100b4ee11e0a0024cd1f0dc2d441fc617ce
Davi <135455745+Davi44128552@users.noreply.github.com>
2024-03-21 19:57:59 -0300

    Add files via upload

---
+++ aula1.java
@@ -0,0 +1,17 @@
+/*  Comandos: \t (Marca parágrafo)
+              \"\" (Põe o texto entre aspas)
+              \\ (Adiciona "\" no texto)
+*/
+
+package Java.aula.aula1;
+
+public class aula1 {
+
+    public static void main(String[] args){
+
+        System.out.print("Hello, world \n");
+        System.out.print("Hello, world");
+
+    }
+
+}
