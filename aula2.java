a740f37d1a6afeaec251914b6870670b6199fd02
Davi <135455745+Davi44128552@users.noreply.github.com>
2024-03-21 19:48:14 -0300

    Add files via upload

---
+++ aula2.java
@@ -0,0 +1,18 @@
+package Java.aula.aula2;
+
+public class aula2 {
+
+    public static void main(String[] args){
+
+        // Adicionando as variáveis
+        int x; // Declarando a variável
+        x = 10; // Definindo valor da variável
+        int y = 25; // Declarando e definindo o valor da variável
+
+        // Printando as variáveis
+        System.out.println(x); 
+        System.out.println(y); 
+
+    }
+    
+}
